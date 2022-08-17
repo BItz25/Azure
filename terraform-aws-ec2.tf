4dbc3d96a7a5806c3013505024e3911893505082
Gonzalo González <gonzalo.gonzalez@afirma.solutions>
2022-08-17 19:15:36 +0000

    Update terraform-aws-ec2.tf

--- terraform-aws-ec2.tf
+++ terraform-aws-ec2.tf
@@ -13,12 +13,16 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "lab_server_01" {
   ami           = "ami-08d4ac5b634553e16"
   instance_type = "t2.micro"
   key_name= "ssh-v1"
-
   tags = {
     Name = "Server_terraform_lab01"
   }
+}
+
+output "instance_public_ip" {
+  description = "Obtener la IP publica de mi instancia"
+  value = aws_instance.lab_server_01.public_ip  // aws_instance.$NOMBRE-RECURSO-TIPO-aws_instance$.public_ip
 }
